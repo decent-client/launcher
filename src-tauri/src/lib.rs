788763d373f714be8b6ad231dced59d9554f6642
Liam Wahlberg <liam.wahlberg07@gmail.com>
2025-03-01 17:37:13 +0100

    Initialized project with Tauri and Vite React

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        // .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
