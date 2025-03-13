8ce17a316a9fb229c7636ca127f70efbde555f20
Liam Wahlberg <liam.wahlberg07@gmail.com>
2025-03-13 19:53:09 +0100

    chore: remove JavaScript lint-staged config, update tsconfig to reference TypeScript config, and modify pre-commit hook to use new config

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,8 +1,6 @@
-/**
- * @filename: lint-staged.config.js
- * @type {import('lint-staged').Configuration}
- */
+import type { Configuration } from "lint-staged";
+
 export default {
   "*.{ts,d.ts,tsx,json}": ["biome check --write --no-errors-on-unmatched"],
   "*.rs": ["cargo fmt --"],
-};
+} satisfies Configuration;
